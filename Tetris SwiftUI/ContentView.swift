8d6a927dcb060c2fa9f7fc07131f5c6957e8a69a
hvaftrudner <krill_eriksson@hotmail.com>
2021-05-26 02:41:26 +0200

    Initial Commit

---
+++ Tetris SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tetris SwiftUI
+//
+//  Created by Kristoffer Eriksson on 2021-05-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
